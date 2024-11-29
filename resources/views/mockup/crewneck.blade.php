a2a707ff54bec1d52470c6168b784baf65085d90
Rayhan Ramadhani <58447376+RayhanRamadhani@users.noreply.github.com>
2024-11-29 23:01:34 +0700

    Rombak all mockup page

--- resources/views/mockup/crewneck.blade.php
+++ resources/views/mockup/crewneck.blade.php
@@ -11,13 +11,13 @@
 </div>
 
 <div class="h-50 mb-2 flex w-fit grid-cols-2 gap-x-1 rounded-lg bg-white p-3 shadow-sm outline outline-1 outline-slate-200 justify-self-center">
-  <div id="kiri" class="grid-rows-auto h-fit w-fit justify-items-center space-y-2 rounded-lg bg-white p-2 shadow-lg hover:divide-solid">
+  <div id="kiri" class="grid-rows-auto h-fit w-fit justify-items-center space-y-2 rounded-lg bg-white p-2 shadow-lg hover:divide-solid outline outline-1 outline-slate-100">
     <div class="rounded-lg bg-slate-50 outline outline-1 outline-slate-200 space-1">
       <button id="newText" class="flex w-full items-center gap-x-2 rounded-lg px-4 py-2 justify-center text-lg text-slate-700 transition duration-300 hover:bg-slate-200"><i class="fa-regular fa-i"></i> Teks</button>
       <form class="flex flex-col">
         <label for="uploadImg" class="w-full cursor-pointer justify-center rounded-lg px-4 py-2 text-lg text-slate-700 hover:bg-slate-200"><i class="fa-solid fa-image"></i> Gambar</label>
-        <input type="file" id="uploadImg" class="hidden" onchange="showFileName(this)" />
-        <span id="fileName" class="text-black"></span>
+        <input type="file" id="uploadImg" class="hidden" multiple onchange="handleFiles(this)" />
+        <ul id="fileList" class="mt-2 list-disc text-black"></ul>
       </form>
       <button id="downImg" class="flex w-full items-center gap-x-2 rounded-lg px-4 py-2 justify-center text-lg text-slate-700 transition duration-300 hover:bg-slate-200"><i class="fa-solid fa-download"></i>  Simpan</button>
     </div>
@@ -28,41 +28,30 @@
     <button class="justify-center flex w-full rounded-lg px-4 py-2 text-lg text-slate-700 outline outline-1 outline-slate-200 transition duration-300 hover:bg-slate-200">
       <a href="{{ route('catalogs.list') }}">Kembali</a>
     </button>
+    <div id="divHapus" class="hidden justify-self-center rounded-lg bg-red-500 px-4 py-2 text-lg font-medium text-white transition duration-300 hover:bg-red-600 hover:text-white shadow-lg">
+      <button id="hapusObj" type="button" onclick="delObj()">Hapus</button>
+    </div>
+
   </div>
 
-  <div id="tengah" class="rounded-lg bg-green-200 shadow-lg">
-    <canvas id="canvas-bg" width="1098" height="720">canvas</canvas>
+  <div id="tengah" class="rounded-lg bg-green-200 shadow-lg pt-6">
+    <canvas id="canvas-bg" width="1098" height="549">canvas</canvas>
   </div>
 </div>
 
-<h1>rayhan</h1>
+<div class="pt-10"></div>
 @endsection
 
 @push('fabric_scripts')
     <script>
         // Upload nama file
-        function validateFile(input) {
-          const file = input.files[0];
-          const fileNameSpan = document.getElementById("fileName");
-          const allowedExtensions = ["png", "jpg", "jpeg"];
-
-          if (file) {
-            const fileExtension = file.name.split('.').pop().toLowerCase();
-            if (allowedExtensions.includes(fileExtension)) {
-              showFileName(input);
-            } else {
-              input.value = "";
-              fileNameSpan.textContent = "Format file tidak didukung! Harus .png, .jpg, atau .jpeg";
-              alert("File tidak valid! Harap unggah file dengan format .png, .jpg, atau .jpeg.");
-            }
-          }
-        }
-
-        function showFileName(input) {
-          const fileNameSpan = document.getElementById("fileName");
-          const file = input.files[0];
-
-          if (file) {
+        function handleFiles(input) {
+        const fileList = document.getElementById("fileList");
+        const file = input.files[0];
+        fileList.innerHTML = "";
+        
+        // Pendekin nama file
+        if (file) {
             const maxLength = 20;
             const fileName = file.name;
 
@@ -76,16 +65,28 @@
           } else {
             fileNameSpan.textContent = "";
           }
+
+        if (input.files.length > 0) {
+          Array.from(input.files).forEach((file, index) => {
+            // Daftar file
+            const listItem = document.createElement("li");
+            listItem.textContent = `${index + 1}. ${file.name}`;
+            fileList.appendChild(listItem);
+          });
+        } else {
+          fileList.textContent = "Tidak ada file dipilih.";
         }
+      }
 
         // Background
         let canvas = new fabric.Canvas("canvas-bg", { 
-          backgroundImage: "{{ asset('images/mockup-tshirt.png') }}",
-                scaleToHeight: 720,
+          backgroundImage: "{{ asset('images/mockup-crewneck.png') }}",
+                scaleToHeight: 549,
                 scaleToWidth: 1098,
         });
 
         // Editor objek
+        const deleteBtn = document.getElementById("hapusObj");
         fabric.Object.prototype.set({
               cornerStyle: 'rect',
               cornerStrokeColor: 'blue',
@@ -96,8 +97,51 @@
               borderColor: 'blue',
               borderDashArray: null,
               borderScaleFactor: 2,
-          });
+        });
 
+        canvas.on('selection:created', function() {
+            const deleteDiv = document.getElementById("divHapus");
+            deleteDiv.classList.remove("hidden");
+        });
+        canvas.on('selection:updated', function() {
+            const deleteDiv = document.getElementById("divHapus");
+            deleteDiv.classList.remove("hidden");
+        });
+        canvas.on('selection:cleared', function() {
+            const deleteDiv = document.getElementById("divHapus");
+            deleteDiv.classList.add("hidden");
+        });
+
+        // Event listener untuk menghapus objek saat tombol hapus diklik
+        deleteBtn.addEventListener('click', function() {
+            const activeObject = canvas.getActiveObject();
+            if (activeObject) {
+                canvas.remove(activeObject);
+                canvas.discardActiveObject();
+                canvas.renderAll();
+                document.getElementById("divHapus").classList.add("hidden");
+            }
+        });
+
+          // Hapus objek
+          function delObj() {
+            const fileInput = document.getElementById("uploadImg");
+            const fileNameSpan = document.getElementById("fileName");
+            var activeObj = canvas.getActiveObject()
+            if (activeObj) {
+              canvas.remove(activeObj)
+              if (activeObj.type == "activeSelection") {
+                activeObj.getObjects().forEach(x => canvas.remove(x))
+                canvas.discardActiveObject().renderAll()
+              }
+            }
+            fileInput.value = "";
+            if (fileNameSpan) {
+              fileNameSpan.textContent = "";
+            }
+          }
+        
+        // Tambah teks ke canvas
         document.getElementById("newText").addEventListener("click", function() {
             console.log('menambahkan teks');
             const newText = new fabric.Textbox('Masukkan teks di sini', {
@@ -110,6 +154,7 @@
             canvas.add(newText);
         });
 
+        // Tambah gambar ke canvas
         document.getElementById('uploadImg').addEventListener("change", function (e) {
             var file = e.target.files[0];
             var reader = new FileReader();
@@ -126,7 +171,8 @@
             };
             reader.readAsDataURL(file);
         });
-
+        
+        // Download gambar
         document.getElementById('downImg').addEventListener('click', function() {
             const dataURL = canvas.toDataURL({ format: 'jpeg', quality: 1 });
             const link = document.createElement('a');
