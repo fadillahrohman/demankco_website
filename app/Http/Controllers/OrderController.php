a435de316b316001da2df9311fb9de28d2316522
Rayhan Ramadhani <58447376+RayhanRamadhani@users.noreply.github.com>
2024-12-10 10:32:58 +0700

    eeee

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -94,11 +94,11 @@ class OrderController extends Controller
             'province_destination' => 'required|exists:provinces,id',
             'city_destination' => 'required|exists:cities,id',
             'address' => 'required|string|max:500',
-            'notes' => 'nullable|string|max:500',
+            // 'notes' => 'nullable|string|max:500',
             'courier' => 'required|string',
             'sizes' => 'required|array',
             'sizes.*' => 'integer|min:0', 
-            'total_price' => 'required|numeric',
+            'total_price' => 'required|numeric|min:0.01',
         ]);
 
         // Simpan data Order
@@ -110,13 +110,34 @@ class OrderController extends Controller
             'list_size' => json_encode($validated['sizes']),
             'total_price' => $validated['total_price'],
             'address' => $validated['address'],
-            'notes' => $validated['notes'],
         ]);
 
-        // Hapus harga sablon dari localStorage setelah pesanan disimpan
-        echo "<script>localStorage.removeItem('hargaSablon');</script>";
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = config('midtrans.serverKey');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
 
-        return redirect()->route('order.success');
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $order->id,
+                'gross_amount' => $order->total_price,
+            ),
+            'customer_details' => array(
+                'first_name' => $order->name,
+                'email' => Auth::user()->email,
+                'phone' => Auth::user()->phone,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        $order->snap_token = $snapToken;
+        $order->save();
+
+        return redirect()->route('orderDetail', $order->id);
     }
     
     
